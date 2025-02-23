c2e44b6b2df9b6ffdda41c836b9531a75137cab6
CrANeZ2430 <crn.alexei@gmail.com>
2025-02-23 18:18:32 +0200

    Change in animalrepos

--- src/Animals.Infrastructure/Core/Domain/Animals/Common/AnimalsRepository.cs
+++ src/Animals.Infrastructure/Core/Domain/Animals/Common/AnimalsRepository.cs
@@ -2,7 +2,6 @@
 using Animals.Core.Domain.Animals.Common;
 using Animals.Core.Domain.Animals.Models;
 using Microsoft.EntityFrameworkCore;
-using System.Threading;
 
 namespace Animals.Infrastructure.Core.Domain.Animals.Common;
 
